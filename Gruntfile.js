2b579aa2bf54949ab59ffd87ac5055594fcd9870
mrghosti3 <mysteriousghst@gmail.com>
2020-03-21 18:46:37 +0200

    added scss compile task

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+const sass = require('node-sass');
+
+require('load-grunt-tasks')(grunt);
+
+grunt.initConfig({
+	sass: {
+		options: {
+			implementation: sass,
+			sourceMap: true
+		},
+		dist: {
+			files: {
+				'style.css': 'style.scss'
+			}
+		}
+	}
+});
+
+grunt.registerTask('default', ['sass']);
