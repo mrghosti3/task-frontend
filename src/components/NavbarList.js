30f724bfe93d82eebab08f74c84c83ca698bf9db
mrghosti3 <mysteriousghst@gmail.com>
2020-03-23 16:01:13 +0200

    Created NavbarList component

---
+++ src/components/NavbarList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class NavbarList extends Component {
+    render() {
+        return (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <a href="#">Web Hosting</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#">
+                        <p>on sale</p>
+                        Cloud Hosting
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a href="#">VPS Hosting</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#">VPS Hosting</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#">Website Builder</a>
+                </li>
+            </ul>
+        );
+    }
+}
+
+NavbarList.propTypes = {
+    menuList: PropTypes.array.isRequired
+}
+
+export default NavbarList;
