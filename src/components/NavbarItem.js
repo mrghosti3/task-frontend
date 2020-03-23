db3be086d4e828bc2dbec0cf8e416a0281088a2d
mrghosti3 <mysteriousghst@gmail.com>
2020-03-23 16:57:59 +0200

    Added NavbarItem.js

---
+++ src/components/NavbarItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NavbarItem extends React.Component {
+    render() {
+        return (
+            <li className="nav-item">
+                    <a href="#">Web Hosting</a>
+            </li>
+        );
+    }
+}
