a2d600f1e73bc276bba49d326f8ebdeb436b25ba
mrghosti3 <mysteriousghst@gmail.com>
2020-03-25 18:01:57 +0200

    Finished Service list rendering

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Service from './Service';
 
 class ServiceList extends React.Component {
     render() {
         return (
             <div class="row d-relative pos-bottom-0">
-                <div class="col">
-                    <p class="m-0">
-                        <i class="fa fa-check text-success mr-10"></i>
-                        Garanteed 99.9% Uptime
-                    </p>
-                </div>
-                <div class="col">
-                    <p class="m-0">
-                        <i class="fa fa-check text-success mr-10"></i>
-                        Superior Speed Performance
-                    </p>
-                </div>
-                <div class="col">
-                    <p class="m-0">
-                        <i class="fa fa-check text-success mr-10"></i>
-                        24/7 Support Chat
-                    </p>
-                </div>
-                <div class="col">
-                    <p class="m-0">
-                        <i class="fa fa-check text-success mr-10"></i>
-                        30-Day Money-Back Guarantee
-                    </p>
-                </div>
+                {this.props.services.map((service, index) => (
+                    <Service key={"service-" + index.toString()} serv={service} />
+                ))}
             </div>
         );
     }
 }
 
 ServiceList.propTypes = {
-    services: PropTypes.object.isRequired
+    services: PropTypes.array.isRequired
 };
 export default ServiceList;
