b1f2b254e75b10f12ea9fed4759e4ab80e90d00c
mrghosti3 <mysteriousghst@gmail.com>
2020-03-26 20:49:23 +0200

    Added grunt-contrib-concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,22 @@
 module.exports = grunt => {
     const sass = require('sass');
+    const scssSrc = 'public/styles/';
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: [
+                    scssSrc + 'global_vars.scss',
+                    scssSrc + 'global_classes.scss',
+                    scssSrc + 'base.scss',
+                    scssSrc + 'navbar.scss',
+                    scssSrc + 'main_content.scss',
+                    scssSrc + 'service.scss'
+                ],
+                dest: scssSrc + 'style.scss'
+            }
+        },
         sass: {
             dist: {
                 files: {
@@ -16,7 +30,8 @@ module.exports = grunt => {
         }
     });
     
+    grunt.loadNpmTasks('grunt-contrib-concat'); 
     grunt.loadNpmTasks('grunt-sass');
     
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['concat', 'sass']);
 };
